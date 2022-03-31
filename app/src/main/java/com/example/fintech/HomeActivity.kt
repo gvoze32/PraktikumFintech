87dc0270ba94f8f42efb86e2bde281609ce1227c
Syafa Adena <gvoze32@gmail.com>
2022-04-01 00:43:46 +0700

    Activity 1

--- app/src/main/java/com/example/fintech/HomeActivity.kt
+++ app/src/main/java/com/example/fintech/HomeActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var tvHome: androidx.appcompat.widget.AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        tvHome = findViewById(R.id.tv_home)
+        var username: String? = intent.getStringExtra("username")
+        tvHome.text = (username)
     }
 }
