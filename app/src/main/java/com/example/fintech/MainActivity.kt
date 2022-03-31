06a22a39bcd954b47d4d1635ec8277751c847bfd
Syafa Adena <gvoze32@gmail.com>
2022-04-01 00:09:46 +0700

    Project Android & Fragment

---
+++ app/src/main/java/com/example/fintech/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.fintech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnHome : android.widget.Button
+    private lateinit var btnProfile : android.widget.Button
+    private lateinit var btnSettings : android.widget.Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnHome = findViewById(R.id.btnHome)
+        btnProfile = findViewById(R.id.btnProfile)
+        btnSettings = findViewById(R.id.btnSettings)
+
+/*        btnHome.setOnClickListener {
+            val intent = android.content.Intent(this, HomeActivity::class.java)
+            intent.putExtra("username", "adena")
+            startActivity(intent)
+        }*/
+
+        btnHome.setOnClickListener {
+            loadFragment(HomeFragment())
+        }
+    }
+    private fun loadFragment(fragment: androidx.fragment.app.Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
+    }
+}
